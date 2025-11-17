15630d6badc2254e8f27958825982ba105527491
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-11-17 11:16:47 -0400

    Initial commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed = 5;
+    public float gravity = -10;
+
+    public float mouseSensitivity = 3;
+
+    private Vector3 velocity;
+
+    private CharacterController characterController;
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        cameraTransform = Camera.main.transform;
+        cameraTransform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        cameraTransform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+        characterController.Move(move * Time.deltaTime); 
+    }
+}
