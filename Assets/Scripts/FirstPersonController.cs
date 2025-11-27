6760472b2fe064f38e3e3b8cc129eef91fa5bad4
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-11-27 12:56:57 -0400

    Movement done

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -6,15 +6,21 @@ public class FirstPersonController : MonoBehaviour
     private CharacterController characterController;
 
     //Variables related to movement
-    public float speed = 5;
+    public float minSpeed = 0;
+    public float speed;
+    public float maxSpeed = 5;
+    public float acceleration = 5;
+    public float deceleration = 10;
 
     //Variables related to jumping
+    public float jumpForce = 2;
     public float gravity = -10;
     private Vector3 velocity;
 
     //Variables related to camera
     private Transform cameraTransform;
     private float offset = 1.5f;
+    public float mouseSensitivity = 5;
 
     void Start()
     {
@@ -31,16 +37,33 @@ public class FirstPersonController : MonoBehaviour
     void Update()
     {
         //Move with WASD
-        float horizontal = Input.GetAxis("Horizontal") * speed;
-        float vertical = Input.GetAxis("Vertical") * speed;
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        characterController.Move(move * Time.deltaTime);
-        
+        Vector3 Direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        Vector3 moveDirection = transform.right * horizontal + transform.forward * vertical;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (Direction.magnitude > minSpeed)
+        {
+            speed = Mathf.MoveTowards(speed, maxSpeed, acceleration * Time.deltaTime);
+            moveDirection = Direction;
+        }
+        else
+        {
+            speed = Mathf.MoveTowards(speed, minSpeed, deceleration * Time.deltaTime);
+        }
+
+        //Move Camera with mouse
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
         //Jump with Spacebar
         if(Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
         {
             
         }
+        
     }
 }
